0adfc00fdd7c17a802b06988c189e7151a3d6cf4
Alexe23b <alexe23b@gmail.com>
2016-06-08 17:56:25 +0300

    the comments were added

--- src/task_1.java
+++ src/task_1.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
  */
 public class task_1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);  //  creation new scanner for reading data from screen
         System.out.println("Input the amount of pairs of brackets");
-        double amount = in.nextInt();
+        double amount = in.nextInt();  //reading integer from console
 
-        double rez = factorial(2 * amount) / (factorial(amount) * factorial(amount + 1));
-        long result = Math.round(rez);
+        double rez = factorial(2 * amount) / (factorial(amount) * factorial(amount + 1));  // calculation with binomial coefficients
+        long result = Math.round(rez);  // rounding
 
-        System.out.println("Amount of pairs of brackets " + (long)amount + ";");
+        System.out.println("Amount of pairs of brackets " + (long) amount + ";");  // input data to console
 
         System.out.println("Amount of the Regular Bracket sequences " + result + ";");
     }
 
-    public static double factorial(double n) {
+    public static double factorial(double n) {  //  method of calculation of factorials
         double res = 1;
         for (double i = 2; i <= n; i = i + 1) res *= i;
         return res;
--- src/task_3.java
+++ src/task_3.java
@@ -7,22 +7,22 @@ public class task_3 {
 
     public static void main(String[] args) throws java.lang.Exception {
 
-        BigInteger fact = BigInteger.ONE;
+        BigInteger fact = BigInteger.ONE; // set value of constant 1 in the format BigInteger
 
         int n = 100;
 
         for (int i = 2; i <= n; i++) {
-            fact = fact.multiply(new BigInteger(String.valueOf(i)));
+            fact = fact.multiply(new BigInteger(String.valueOf(i)));  // calculation N!
         }
         System.out.println(n + "! = " + fact);
 
-        String s = "" + fact;
+        String s = "" + fact;   // concatenation string with BigInteger automatically parse to string
         int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            sum += Integer.parseInt(s.substring(i, i + 1));
+        for (int i = 0; i < s.length(); i++) {  // cycle "for" with using length of string as exit parameter
+            sum += Integer.parseInt(s.substring(i, i + 1));  //   getting symbols as substring, parsing them to integer and sum
         }
 
-        System.out.println("sum = " + sum);
+        System.out.println("sum = " + sum);   //input res to console
     }
 }
 
